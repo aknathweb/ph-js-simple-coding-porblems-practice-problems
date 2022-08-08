c374e49a675dc79b13741766faa6883a6db2a0ca
aknathweb <aniknwu.cse@gmail.com>
2022-08-09 00:08:55 +0600

    rectangle area

---
+++ rectangle_area.js
@@ -0,0 +1,8 @@
+// ৪. একটা ফাংশন লিখো। যেটা ইনপুট প্যারামিটার হিসেবে একটা আয়তক্ষেত্রের দৈর্ঘ্য আর উচ্চতাকে নিবে। তারপর সেই আয়তক্ষেত্র এর area (আয়তন) কে রেজাল্ট হিসেবে রিটার্ন করবে।
+
+function RectangleArea(length, height) {
+    const RectangleArea = length * height;
+    return RectangleArea;
+}
+const length = 40, height = 5;
+console.log("Area of Rectangle : ", RectangleArea(length, height));
